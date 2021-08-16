6b53e382356209602faa4403fa9e65b8f863239e
Menake Ratnayake <Menake.Ratnayake@beca.com>
2021-08-16 22:31:26 +1200

    use cors proxy

--- hooks/usePrices.js
+++ hooks/usePrices.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 
+const corsProxyUrl = 'http://fathomless-scrubland-94941.herokuapp.com/'
+
 export const usePrices = () => {
     const [prices, setPrices] = useState([])
 
     const fetchPrices = async () => {
-        const response = await fetch('https://feeds.companyresearch.nzx.com/data/prices.php?format=JSON');
+        const response = await fetch(`${corsProxyUrl}https://feeds.companyresearch.nzx.com/data/prices.php?format=JSON`);
         const json = await response.json();
         setPrices(json.prices);
     }
--- pages/test.js
+++ pages/test.js
@@ -5,7 +5,7 @@ export default function Index() {
 
     return (
         <pre>
-            {prices}
+            {JSON.stringify(prices)}
         </pre>
     )
 }
